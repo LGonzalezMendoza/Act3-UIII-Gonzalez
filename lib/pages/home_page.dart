1757049bc0a2d6ec36242e7184bea3cb64d9f1c1
LEONARDO_GONZALEZ_MENDOZA <a.21308051280490@cbtis128.edu.mx>
2024-06-06 18:41:40 +0000

    actua

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:Gonzalez0490/pages/list_page.dart';
+import 'package:Gonzalez0490/pages/upload_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _pages = [
+    const PersonList(),
+    const UploadPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Guitar Music Shop'),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
